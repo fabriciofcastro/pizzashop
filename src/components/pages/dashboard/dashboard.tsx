7b5bcf5d7bc0d78174497eccf3decf509022a6e9
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-07-17 00:26:26 -0300

    cards informations Created 'n montn of dashboard

--- src/components/pages/dashboard/dashboard.tsx
+++ src/components/pages/dashboard/dashboard.tsx
@@ -1,9 +1,27 @@
 import { Helmet } from "react-helmet-async";
+import { MonthOrdersAmountCard } from "./month-orders-amount-card";
+import { MonthRevenueCard } from './month-revenue-card'
+import { DayOrdersAmountCard } from "./day-orders-amount-card";
+import { MouthCanceledOrdersAmount } from "./mouth-canceled-orders-amount";
 
 export function Dashboard() {
   return (
-    <Helmet >
-      <title>Dashboard</title>
-    </Helmet>
+    <>
+      <Helmet >
+        <title>Dashboard</title>
+
+      </Helmet>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight"> Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+         <MonthRevenueCard />
+         <MonthOrdersAmountCard />
+         <DayOrdersAmountCard />
+         <MouthCanceledOrdersAmount />
+        </div>
+      </div>
+    </>
+
   )
 }
